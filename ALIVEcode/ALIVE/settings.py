1c2ea64135ea91b5aa0a55f7f3ba6ec20fd7fa57
Enric Soldevila <mosk@DESKTOP-LHSGEE4>
2021-06-20 16:53:48 -0400

    Settings.py changed for deployment

--- ALIVEcode/ALIVE/settings.py
+++ ALIVEcode/ALIVE/settings.py
@@ -10,9 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
 from decouple import config, UndefinedValueError
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ALIVE.settings")
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
