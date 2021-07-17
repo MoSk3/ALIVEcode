4e9c69f880b9951a54f58b39a3aec15667f95158
Enric Soldevila <mosk@DESKTOP-LHSGEE4>
2021-07-17 16:21:27 -0400

    python backend classrooms

--- frontend/src/Pages/Challenge/Challenge.tsx
+++ frontend/src/Pages/Challenge/Challenge.tsx
@@ -1,15 +1,17 @@
 import FillContainer from '../../Components/MiscComponents/FillContainer/FillContainer';
 import { ChallengeProps } from './challengeTypes';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import { useAlert } from 'react-alert';
 import LineInterface from '../../Components/PlayComponents/LineInterface/LineInterface';
+import { UserContext } from '../../UserContext';
 
 const Challenge = (props: ChallengeProps) => {
   const alert = useAlert();
   const history = useHistory();
-  const [challenge, setChallenge] = useState();
+  const [challenge, setChallenge] = useState<any>();
+  const { user } = useContext(UserContext);
 
   useEffect(() => {
     const loadChallenge = async () => {
@@ -25,31 +27,34 @@ const Challenge = (props: ChallengeProps) => {
 
   return (
     <FillContainer>
+      {/* 
       <div className="row h-100">
-        <div className="col-6" style="resize: both; padding:0; display: flex; flex-flow: column;">
-          <div style="flex: 0 1 70px; background-color: #013677; border: none;">
+        <div className="col-6" style={{ resize: 'both', padding: '0', display: 'flex', flexFlow: 'column', }}>
+          <div style={{ flex: '0 1 70px', backgroundColor: '#013677', border: 'none' }}>
             <div id="go-back-button" className="btn btn-primary"
-              onclick="window.location=`{% url 'home:switch_to_bloc_interface' challengeId=challenge.id %}`">
+              onClick={() => "window.location=`{% url 'home:switch_to_bloc_interface' challengeId=challenge.id %}`"}>
               Programmation en blocs
             </div>
             <button type="button" id="btn-book" className="btn" data-toggle="modal" data-target="#modal">
-              <i className="fas fa-book-open fa-2x" style="color: white;"></i>
+              <i className="fas fa-book-open fa-2x" style={{ color: 'white' }}></i>
             </button>
             <button type="button" id="btn-hint" className="btn" data-toggle="modal" data-target="#modal-hint">
-              <i className="fas fa-question-circle fa-2x" style="color: white;"></i>
+              <i className="fas fa-question-circle fa-2x" style={{ color: 'white' }}></i>
             </button>
-            {% if creator %}
-            <input type="text" id="input-challenge-name" value="{{ challenge.name }}" style="margin-left: 5px;">
-              <div id="status-modify-div" className="btn">
-                <label id="saving-status" style="color:white">Niveau sauvegardé ✔</label>
-                <button type="button" id="btn-modify" ­­ className="btn btn-primary" data-toggle="modal"
+
+
+            {user?.professor == challenge?.creator ? (
+              <input type="text" id="input-challenge-name" value="{{ challenge.name }}" style={{ marginLeft: '5px' }}>
+                <div id="status-modify-div" className="btn">
+                  <label id="saving-status" style="color:white">Niveau sauvegardé ✔</label>
+                  <button type="button" id="btn-modify" ­­ className="btn btn-primary" data-toggle="modal"
                         data-target="#edit-modal">
-                <i className="fas fa-cog fa-2x"></i>
+                  <i className="fas fa-cog fa-2x"></i>
                     </button>
                 </div>
-            {% else %}
-            <label id="label-challenge-name" style="color:white; margin-left: 5px;">{{ challenge.name }}</label>
-            {% endif %}
+            ) : (
+              <label id="label-challenge-name" style={{color: 'white', marginLeft: '5px'}}>{{ challenge.name }}</label>
+            )}
           </div>
 
 
@@ -69,6 +74,7 @@ const Challenge = (props: ChallengeProps) => {
         </div>
       </div>
       <LineInterface />
+            */}
     </FillContainer>
   )
 }
