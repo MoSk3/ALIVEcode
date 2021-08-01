b3de0f6a379b4eb7858e99d2f55802cec4802222
Enric Soldevila <mosk@DESKTOP-LHSGEE4>
2021-08-01 18:00:31 -0400

    FIX - indents

--- frontend/src/Components/MainComponents/Footer/Footer.tsx
+++ frontend/src/Components/MainComponents/Footer/Footer.tsx
@@ -5,50 +5,50 @@ import { useTranslation } from 'react-i18next';
 
 const Footer = (props: FooterProps) => {
 
-    const { t } = useTranslation();
+	const { t } = useTranslation();
 
-    return (
-        <footer className="site-footer">
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-12 col-md-6">
-                        <h6>{t('home.footer.about.title')}</h6>
-                        <p className="text-justify">
-                            {t('home.footer.about.description')}
-                        </p>
-                    </div>
+	return (
+		<footer className="site-footer">
+			<div className="container">
+				<div className="row">
+					<div className="col-sm-12 col-md-6">
+						<h6>{t('home.footer.about.title')}</h6>
+						<p className="text-justify">
+							{t('home.footer.about.description')}
+						</p>
+					</div>
 
-                    <div className="col-xs-6 col-md-3">
-                        <h6>{t('home.footer.categories')}</h6>
-                        <ul className="footer-links">
-                            <li><Link to="/dashboard">{t('msg.section.dashboard')}</Link></li>
-                            <li><Link to="/mind">{t('msg.section.amc')}</Link></li>
-                            <li><Link to="/about">{t('msg.section.about')}</Link></li>
-                        </ul>
-                    </div>
+					<div className="col-xs-6 col-md-3">
+						<h6>{t('home.footer.categories')}</h6>
+						<ul className="footer-links">
+							<li><Link to="/dashboard">{t('msg.section.dashboard')}</Link></li>
+							<li><Link to="/mind">{t('msg.section.amc')}</Link></li>
+							<li><Link to="/about">{t('msg.section.about')}</Link></li>
+						</ul>
+					</div>
 
-                    <div className="col-xs-6 col-md-3">
-                        <h6>{t('home.footer.links')}</h6>
-                        <ul className="footer-links">
-                            <li><a href="https://lrima.cmaisonneuve.qc.ca/" rel="noopener noreferrer" target="_blank">LRIMA</a></li>
-                            <li><a href="https://lrima.cmaisonneuve.qc.ca/alive/" rel="noopener noreferrer" target="_blank">LRIMA ALIVE</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <hr />
-            </div>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-8">
-                        {/* TODO: copyright logo*/}
-                        <p className="copyright-text">{t('home.footer.copyright')}
-                            <a href="https://lrima.cmaisonneuve.qc.ca/" rel="noopener noreferrer" target="_blank"> LRIMA</a>.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    )
+					<div className="col-xs-6 col-md-3">
+						<h6>{t('home.footer.links')}</h6>
+						<ul className="footer-links">
+							<li><a href="https://lrima.cmaisonneuve.qc.ca/" rel="noopener noreferrer" target="_blank">LRIMA</a></li>
+							<li><a href="https://lrima.cmaisonneuve.qc.ca/alive/" rel="noopener noreferrer" target="_blank">LRIMA ALIVE</a></li>
+						</ul>
+					</div>
+				</div>
+				<hr />
+			</div>
+			<div className="container">
+				<div className="row">
+					<div className="col-md-8">
+						{/* TODO: copyright logo*/}
+						<p className="copyright-text">{t('home.footer.copyright')}
+							<a href="https://lrima.cmaisonneuve.qc.ca/" rel="noopener noreferrer" target="_blank"> LRIMA</a>.
+						</p>
+					</div>
+				</div>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer;
