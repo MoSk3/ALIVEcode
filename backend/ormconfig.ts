f9785a33b4ecda371eeaed13cff9cb9139cff515
Enric Soldevila <mosk@DESKTOP-LHSGEE4>
2021-08-16 16:08:45 -0400

    MERGE

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -3,7 +3,7 @@ import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionO
 const config: SqliteConnectionOptions = {
   type: 'sqlite',
   database: 'db.sqlite3',
-  entities: ['dist/src/**/entities/*.entity.js', 'node_modules/nestjs-admin/**/*.entity.js'],
+  entities: ['dist/src/**/entities/*.entity.js', 'dist/src/**/**/entities/*.entity.js', 'node_modules/nestjs-admin/**/*.entity.js'],
   synchronize: true,
   migrations: ['dist/src/db/migrations/*.js'],
   cli: {
