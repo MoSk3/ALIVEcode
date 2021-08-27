fec4b95a34a83ad67fc8b99d17659d31cac9b2c0
Soldevila <e2059059@cmaisonneuve.qc.ca>
2021-08-27 15:53:52 -0400

    ADD - Start of about page

--- frontend/src/Pages/About/About.tsx
+++ frontend/src/Pages/About/About.tsx
@@ -1,10 +1,32 @@
 import { AboutProps } from './aboutTypes';
 import FillContainer from '../../Components/UtilsComponents/FillContainer/FillContainer';
+import CardContainer from '../../Components/UtilsComponents/CardContainer/CardContainer';
+import { useTranslation } from 'react-i18next';
+import AboutCard from '../../Components/UtilsComponents/Cards/AboutCard/AboutCard';
+import { Row } from 'react-bootstrap';
 
 const About = (props: AboutProps) => {
+	const { t } = useTranslation();
+
 	return (
 		<FillContainer startAtTop centered>
-			<h1>In work...</h1>
+			<CardContainer title={t('msg.section.about')}>
+				<div>
+					<Row>
+						<AboutCard name="Jihène Rezgui" img="lol" />
+					</Row>
+					<Row>
+						<AboutCard name="Jihène Rezgui" img="lol" />
+						<AboutCard name="Jihène Rezgui" img="lol" />
+						<AboutCard name="Jihène Rezgui" img="lol" />
+						<AboutCard name="Jihène Rezgui" img="lol" />
+						<AboutCard name="Jihène Rezgui" img="lol" />
+						<AboutCard name="Jihène Rezgui" img="lol" />
+						<AboutCard name="Jihène Rezgui" img="lol" />
+						<AboutCard name="Jihène Rezgui" img="lol" />
+					</Row>
+				</div>
+			</CardContainer>
 		</FillContainer>
 	);
 };
