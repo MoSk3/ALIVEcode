229d9e5c712a28d2aba89a4fbcae20f8927c72d8
MC <malik.c@hotmail.fr>
2021-09-23 11:31:17 -0400

    Squelette de la page
    Ajout des progress circles

--- frontend/src/Pages/Account/AccountInfo/AccountPage.tsx
+++ frontend/src/Pages/Account/AccountInfo/AccountPage.tsx
@@ -3,40 +3,145 @@ import { useContext } from 'react';
 import { UserContext } from '../../../state/contexts/UserContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import CenteredContainer from '../../../Components/UtilsComponents/CenteredContainer/CenteredContainer';
+import FillContainer from '../../../Components/UtilsComponents/FillContainer/FillContainer';
 import { Professor, Student } from '../../../Models/User/user.entity';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import './accountPage.css'
+import CardContainer from '../../../Components/UtilsComponents/CardContainer/CardContainer';
+import { Container, Row } from 'react-bootstrap';
+import styled from 'styled-components';
+import AboutCard from '../../../Components/UtilsComponents/Cards/AboutCard/AboutCard';
+
+
+const StyledCenteredContainer = styled(CenteredContainer)`
+	padding: 0 10% 0 10%;
+
+	.row-prof {
+		margin-top: 10px;
+		margin-bottom: 10px;
+	}
+`;
 
 const AccountPage = () => {
 	const { user } = useContext(UserContext);
+	const percentage = 33;
+	const percentageIOT = 77;
+	const percentage2 = 12;
+	const percentage3 = 88;
+	const percentage4 = 57;
 
 	return (
-		<CenteredContainer horizontally vertically textAlign="center">
-			{!user ? (
-				<FontAwesomeIcon icon={faSpinner} />
-			) : (
-				<div style={{ padding: '20px' }}>
-					<h1>{user.getDisplayName()}</h1>
+		<>
 
-					{user instanceof Professor && (
-						<>
-							<label>Nom</label>
-							<br />
-							<input defaultValue={user.firstName} />
-							<br />
-							<label>Nom de famille</label>
-							<br />
-							<input defaultValue={user.lastName} />
-							<br />
-						</>
-					)}
-					{user instanceof Student && (
-						<>
-							<label>Nom</label>
-							<input defaultValue={user.name} />
-						</>
-					)}
-				</div>
-			)}
-		</CenteredContainer>
+			<StyledCenteredContainer>
+				<Row>
+					<div className="col-md-6">
+						<div className="pb-5">
+							<CardContainer title="Profil">
+								
+								{!user ? (
+									<FontAwesomeIcon icon={faSpinner} />
+								) : (
+									<Row >
+										
+										<AboutCard name= {user.getDisplayName()} img= "https://i.imgur.com/xkH6wCg.png"/>
+								
+										{user instanceof Professor && (
+											<>
+												<label>Nom</label>
+												<br />
+												<input defaultValue={user.firstName} />
+												<br />
+												<label>Nom de famille</label>
+												<br />
+												<input defaultValue={user.lastName} />
+												<br />
+											</>
+										)}
+										<div  className="text-left col-sm-6">
+											<h2>Description</h2>
+											<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eleifend tempus consequat. 
+												Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.	
+											</h3>
+									</div>
+									</Row>
+
+
+								)}	
+								{user instanceof Student && (
+											<>
+												<label>Nom</label>
+												<input defaultValue={user.name} />
+											</>
+										)}
+							</CardContainer>
+						</div>
+					</div>
+					<div className="col-md-5  offset-md-1">
+						<CardContainer title="Avancement">
+							
+							<Row>
+								<h3 className="text-left col-sm-5">Quiz: 0/10</h3>
+								<h3 className="text-left col-sm-7">Récompenses: 10/40</h3>
+								<h3 className="text-left col-sm-5">Status: Noob</h3>
+								<h3 className="text-left col-sm-7">Rank #1</h3>
+								<h3 className="text-left col-sm-5">Posts: 0</h3>
+								<h3 className="text-left col-sm-7">Autre</h3>
+							</Row>
+						</CardContainer>
+					</div>
+
+				</Row>
+			</StyledCenteredContainer>
+			<StyledCenteredContainer>								
+				<CardContainer title="Quiz" >
+					<Row style= {{paddingTop: '20px'}}>
+						
+						<div className=" col-sm-2">
+							<CircularProgressbar value={percentageIOT} text={`${percentageIOT}%`}/>
+							<h3>IOT</h3>
+						</div>
+						<div className=" col-sm-2">
+							<CircularProgressbar value={percentage} text={`${percentage}%`}/>
+						</div>
+						<div className=" col-sm-2">
+							<CircularProgressbar value={percentage2} text={`${percentage2}%`}/>
+						</div>
+						<div className=" col-sm-2">
+							<CircularProgressbar value={percentage3} text={`${percentage3}%`}/>
+						</div>
+						<div className=" col-sm-2">
+							<CircularProgressbar value={percentage4} text={`${percentage4}%`}/>
+						</div>
+					</Row>
+				</CardContainer>
+			</StyledCenteredContainer>
+
+			<StyledCenteredContainer>
+				<CardContainer title="Recompenses" >
+					<Row style={{ padding: '20px' , }}>
+						<div style={{ paddingRight: '20px' , }}>
+							<img height={100} src="https://i.imgur.com/xkH6wCg.png" />
+						</div>
+						<div style={{ paddingRight: '20px' , }}>
+							<img height={100} src="https://i.imgur.com/xkH6wCg.png" />
+						</div>
+						<div style={{ paddingRight: '20px' , }}>
+							<img height={100} src="https://i.imgur.com/xkH6wCg.png" />
+						</div>
+						<div style={{ paddingRight: '20px' , }}>
+							<img height={100} src="https://i.imgur.com/xkH6wCg.png" />
+						</div>
+						<div style={{ paddingRight: '20px' , }}>
+							<img height={100} src="https://i.imgur.com/xkH6wCg.png" />
+						</div>
+						<div>
+							<img height={100} src="https://i.imgur.com/xkH6wCg.png" />
+						</div>
+					</Row>
+				</CardContainer>
+			</StyledCenteredContainer>
+		</>
 	);
 };
 
