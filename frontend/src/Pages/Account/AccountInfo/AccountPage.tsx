d76eb5c6571100669deed911c3bdb5e84825ae3b
MC <malik.c@hotmail.fr>
2021-09-23 12:05:04 -0400

    color circularProgressBar
    Ajout des couleurs en fonction du pourcentage

--- frontend/src/Pages/Account/AccountInfo/AccountPage.tsx
+++ frontend/src/Pages/Account/AccountInfo/AccountPage.tsx
@@ -3,12 +3,11 @@ import { useContext } from 'react';
 import { UserContext } from '../../../state/contexts/UserContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import CenteredContainer from '../../../Components/UtilsComponents/CenteredContainer/CenteredContainer';
-import FillContainer from '../../../Components/UtilsComponents/FillContainer/FillContainer';
 import { Professor, Student } from '../../../Models/User/user.entity';
-import { CircularProgressbar } from 'react-circular-progressbar';
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
 import './accountPage.css'
 import CardContainer from '../../../Components/UtilsComponents/CardContainer/CardContainer';
-import { Container, Row } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 import styled from 'styled-components';
 import AboutCard from '../../../Components/UtilsComponents/Cards/AboutCard/AboutCard';
 
@@ -29,6 +28,11 @@ const AccountPage = () => {
 	const percentage2 = 12;
 	const percentage3 = 88;
 	const percentage4 = 57;
+	const color = (value: any) => {
+		if (value >60) return "gold"
+	else if (value > 30) return "orange"
+	else return "red" 
+	} 
 
 	return (
 		<>
@@ -60,7 +64,8 @@ const AccountPage = () => {
 										)}
 										<div  className="text-left col-sm-6">
 											<h2>Description</h2>
-											<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eleifend tempus consequat. 
+											<h3>
+												Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eleifend tempus consequat. 
 												Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.	
 											</h3>
 									</div>
@@ -98,20 +103,20 @@ const AccountPage = () => {
 					<Row style= {{paddingTop: '20px'}}>
 						
 						<div className=" col-sm-2">
-							<CircularProgressbar value={percentageIOT} text={`${percentageIOT}%`}/>
+							<CircularProgressbar value={percentageIOT} text={`${percentageIOT}%`} styles={buildStyles({trailColor: color(percentageIOT)})} />
 							<h3>IOT</h3>
 						</div>
 						<div className=" col-sm-2">
-							<CircularProgressbar value={percentage} text={`${percentage}%`}/>
+							<CircularProgressbar value={percentage} text={`${percentage}%`}  styles={buildStyles({trailColor: color(percentage)})}/>
 						</div>
 						<div className=" col-sm-2">
-							<CircularProgressbar value={percentage2} text={`${percentage2}%`}/>
+							<CircularProgressbar value={percentage2} text={`${percentage2}%`}  styles={buildStyles({trailColor: color(percentage2)})}/>
 						</div>
 						<div className=" col-sm-2">
-							<CircularProgressbar value={percentage3} text={`${percentage3}%`}/>
+							<CircularProgressbar value={percentage3} text={`${percentage3}%`}  styles={buildStyles({trailColor: color(percentage3)})}/>
 						</div>
 						<div className=" col-sm-2">
-							<CircularProgressbar value={percentage4} text={`${percentage4}%`}/>
+							<CircularProgressbar value={percentage4} text={`${percentage4}%`}  styles={buildStyles({trailColor: color(percentage4)})}/>
 						</div>
 					</Row>
 				</CardContainer>
