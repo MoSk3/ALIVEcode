17938fc81125ea1ee876751a2754d25b232f8ce5
Enric <mosk3002@gmail.com>
2021-09-27 15:53:02 -0400

    MOD - Activity model

--- backend/src/models/course/course.module.ts
+++ backend/src/models/course/course.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CourseEntity } from './entities/course.entity';
 import { SectionEntity } from './entities/section.entity';
 import { UserEntity } from '../user/entities/user.entity';
+import { ActivityEntity } from './entities/activity.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CourseEntity, SectionEntity, UserEntity])],
+  imports: [TypeOrmModule.forFeature([CourseEntity, SectionEntity, ActivityEntity, UserEntity])],
   controllers: [CourseController],
   providers: [CourseService],
 })
