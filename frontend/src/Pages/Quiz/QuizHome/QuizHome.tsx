f0946d7d7111960839aa346053c4c1eb0c146fd9
Maxime Gagniere <e1967067@cmaisonneuve.qc.ca>
2021-10-01 15:34:09 -0400

    Removed unused imports

--- frontend/src/Pages/Quiz/QuizHome/QuizHome.tsx
+++ frontend/src/Pages/Quiz/QuizHome/QuizHome.tsx
@@ -1,11 +1,7 @@
-import { useState, useContext } from 'react';
-import { UserContext } from '../../../state/contexts/UserContext';
 import useRoutes from '../../../state/hooks/useRoutes';
-import Button from '../../../Components/UtilsComponents/Button/Button';
 import { useHistory } from 'react-router';
 import CenteredContainer from '../../../Components/UtilsComponents/CenteredContainer/CenteredContainer';
 import CardContainer from '../../../Components/UtilsComponents/CardContainer/CardContainer';
-import { Card } from 'react-bootstrap';
 import SmallCard from '../../../Components/UtilsComponents/Cards/SmallCard/SmallCard';
 
 const QuizHome = () => {
@@ -20,7 +16,7 @@ const QuizHome = () => {
                 style={{ paddingLeft: '250px', paddingRight: '250px' }}    
                 >
                 <CardContainer asRow style={{ marginBottom: '100px' }} title="Quiz">
-					<SmallCard to="/quiz" title="IoT" />
+					<SmallCard to="/quiz" title="IoT"/>
 					<SmallCard to="/quiz/new" title="Base de données" />
 					<SmallCard to="/quiz/browse" title="Pseudocode" />
                     <SmallCard to="/quiz/browse" title="Réseaux" />
