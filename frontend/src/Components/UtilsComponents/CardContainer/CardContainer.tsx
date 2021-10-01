1948ce537eb726aa863923e3287abf6ce74e2cc1
Enric Soldevila <62816157+MoSk3@users.noreply.github.com>
2021-09-30 20:22:30 -0400

    Courses models (#47)
    * MOD - Activity model
    
    * ADD - Course add button in classroom
    
    * MOD - Colors of course navigation

--- frontend/src/Components/UtilsComponents/CardContainer/CardContainer.tsx
+++ frontend/src/Components/UtilsComponents/CardContainer/CardContainer.tsx
@@ -42,6 +42,24 @@ const StyledDiv = styled.div`
 	}
 `;
 
+/**
+ * Container used to display cards components in a grid
+ *
+ * @param title title in the header of the component
+ * @param titleSize css size of the title (optional)
+ * @param icon icon displayed after the title (optional)
+ * @param height minimumHeight that takes the component (optional)
+ * @param asRow adds a row that wraps around the children (optional)
+ * @param scrollX css scrollX property (optional)
+ * @param scrollY css scrollY property (optional)
+ * @param onIconClick function that triggers onClick of icon (optional)
+ * @param style tsx style property (optional)
+ * @param className (optional)
+ * @param children
+ * @returns tsx element
+ *
+ * @author Enric
+ */
 const CardContainer = ({
 	children,
 	className,
