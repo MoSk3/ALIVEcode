b6735533c25eb2ef4f0d625290cf63db2916eada
Maxime Gagniere <e1967067@cmaisonneuve.qc.ca>
2021-10-06 09:32:41 -0400

    Quiz Category Squelette

--- frontend/src/Pages/Quiz/QuizCategory/QuizCategory.tsx
+++ frontend/src/Pages/Quiz/QuizCategory/QuizCategory.tsx
@@ -1,4 +1,5 @@
-import { Card } from "react-bootstrap";
+import { Button, Card, Col, Container, Form, Row, Table } from "react-bootstrap";
+import SearchBar from "../../../Components/MainComponents/BrowsingMenu/SearchBar/SearchBar";
 import CenteredContainer from "../../../Components/UtilsComponents/CenteredContainer/CenteredContainer";
 import { QuizCategoryProps } from "./Category";
 
@@ -12,10 +13,69 @@ const QuizHome = (props: QuizCategoryProps) => {
                 style={{ paddingLeft: '250px', paddingRight: '250px' }}    
             >
                 <Card>
-                    <h1> Test </h1>
                     <Card.Body>
-                        test { props.match.params.id  }
+                        {/*
+                            Hardcoded text will be replaced once backend is ready 
+                         */}
+                        <Card.Title>Pseudo Code </Card.Title>
+                        <Card.Text>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione maiores vel, minima nemo neque incidunt? Excepturi ducimus accusantium est accusamus fugiat. Nulla atque iste impedit tenetur, optio libero recusandae ex.
+                        </Card.Text>
                     </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Title>
+                        <Container>
+                            <Row>
+                                <Col>15 Quiz</Col>
+                                <Col></Col>
+                                <Col>
+                                    <Button>Ajouter un Quiz</Button>
+                                </Col>
+                                <Col>
+                                    <Form>
+                                        <Form.Group>
+                                            <Form.Control type="text" placeholder="Search"/>
+                                        </Form.Group>
+                                    </Form>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </Card.Title>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                            <th>Validation</th>
+                            <th>Nom</th>
+                            <th> # de Questions</th>
+                            <th>Auteur</th>
+                            <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <td>Yes</td>
+                            <td>Les fonctions</td>
+                            <td>20</td>
+                            <td>Bob</td>
+                            <td>11-09-2021</td>
+                            </tr>
+                            <tr>
+                            <td>Yes</td>
+                            <td>Les fonctions</td>
+                            <td>20</td>
+                            <td>Bob</td>
+                            <td>11-09-2021</td>
+                            </tr>
+                            <tr>
+                            <td>Yes</td>
+                            <td>Les fonctions</td>
+                            <td>20</td>
+                            <td>Bob</td>
+                            <td>11-09-2021</td>
+                            </tr>
+                        </tbody>
+                    </Table>
                 </Card>
 			</CenteredContainer>
 
