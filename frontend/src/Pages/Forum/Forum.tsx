07962d73fb454b853e5d42b17d14847b565133ed
Benoit <benoitspm@outlook.com>
2021-10-06 09:55:44 -0400

    Update page forum
    Changement bulma to bootstrap

--- frontend/src/Pages/Forum/Forum.tsx
+++ frontend/src/Pages/Forum/Forum.tsx
@@ -1,110 +1,98 @@
-import { useState, useContext } from 'react';
-import { UserContext } from '../../state/contexts/UserContext';
-import useRoutes from '../../state/hooks/useRoutes';
 import Button from '../../Components/UtilsComponents/Button/Button';
 import CenteredContainer from '../../Components/UtilsComponents/CenteredContainer/CenteredContainer';
 import CardContainer from '../../Components/UtilsComponents/CardContainer/CardContainer';
-import { useHistory } from 'react-router';
-import { StyledLevelBrowse } from '../Level/LevelBrowse/levelBrowseTypes';
 
 const Forum = () => {
-	const { routes } = useRoutes();
-	const history = useHistory();
 
 	return (
 		<div>
-			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"/>
-
             <CenteredContainer
 				horizontally
 				textAlign="center"
 				style={{ paddingLeft: '100px', paddingRight: '100px' }}
 			>
 			<div>
-			<nav className="navbar" role="navigation" aria-label="main navigation">
-				<div className="navbar-start">
-					<a className="navbar-item" href="https://bulma.io/">
-						Forum
-					</a>
-					<a className="navbar-item" href="https://bulma.io/">
-						Chat
-					</a>
-					<a className="navbar-item" href="https://bulma.io/">
-						Alive
-					</a>
-				</div>
-				<div className="navbar-end">
-					<input className="input" type="text" name="SearchBar" value="Search"/>
+			<nav className="navbar  navbar-expand-lg navbar-light bg-light">
+				<div className="container-fluid">
+					<div className="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+							<li className="nav-item">
+								<a className="nav-link" href="#">Forum</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#">Chat</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#">Alive</a>
+							</li>
+						</ul>
+					</div>
+					<form className="d-flex">
+        				<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        				<button className="btn btn-light" type="submit">Recherche</button>
+      				</form>
 				</div>
 			</nav>
 			</div>
-			<div className="has-text-left ml-5">
+			<div className="text-left ml-4">
 				Home
 			</div>
 
-			<div className="columns">
-				<div className="column is-8">
-					<CardContainer asRow title="Forums">
-							<div className="has-text-left ml-3">
+			<div className="row">
+				<div className="col-8">
+					<CardContainer asRow title="Forum">
+							<div className="text-left ml-3">
 							Règles<br/>
 							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
 							</div>
 					</CardContainer>
 					<CardContainer asRow title="Iot">
-						<div className="columns is-vcentered">
-							<div className="column is-4">
+						<div className="row">
+							<div className="col border-right border-dark">
 								<div className="media">
-									<div className="media-left">
-										<figure className="image is-48x48">
-											<img className="is-rounded" src="https://bulma.io/images/placeholders/128x128.png" alt=""/>
-										</figure>
-									</div>
-									<div className="has-text-left">
+									<img className="mr-3 rounded-circle" src="https://bulma.io/images/placeholders/64x64.png" alt=""/>
+									<div className="text-left">
 										Iot
 									</div>
 								</div>
 							</div>
-							<div className="column is-5">
-								<div className="has-text-left">
+							<div className="col border-right border-dark">
+								<div className="text-left">
 									111 sujets
 								</div>
-								<div className="has-text-left">
+								<div className="text-left">
 									2322 messages
 								</div>
 							</div>
-							<div className="column is-5 is-offset-6">
-								<div className="has-text-left">
+							<div className="col">
+								<div className="text-left">
 									date dernier post
 								</div>
-							</div>
+							</div>	
 						</div>
 					</CardContainer>
 
-					<CardContainer asRow title="Prog">
+					<CardContainer asRow title="Programmation">
 						<div>
-							<div className="columns">
-								<div className="column">
-									<div className="media">
-										<div className="media-left">
-											<figure className="image is-48x48">
-												<img className="is-rounded" src="https://bulma.io/images/placeholders/128x128.png" alt=""/>
-											</figure>
-										</div>
-										<div className="has-text-left">
-											Javascript
-										</div>
+							<div className="row">
+								<div className="col border-right border-dark">
+								<div className="media">
+									<img className="mr-3 rounded-circle" src="https://bulma.io/images/placeholders/64x64.png" alt=""/>
+									<div className="text-left">
+										Javascript
 									</div>
 								</div>
-								<div className="column is-5">
-									<div className="has-text-left">
+							</div>
+								<div className="col  border-right border-dark">
+									<div className="text-left">
 										111 sujets
 									</div>
-									<div className="has-text-left">
+									<div className="text-left">
 										2322 messages
 									</div>
 								</div>
-								<div className="column is-5 is-offset-6">
-								<div className="has-text-left">
+								<div className="col">
+								<div className="text-left">
 									date dernier post
 								</div>
 							</div>
@@ -113,29 +101,24 @@ const Forum = () => {
 					</CardContainer>
 				</div>
 				
-				<div className="column">
-					<div className="column pt-6">
-						<button className="button is-medium is-fullwidth">Créer un sujet</button>
+				<div className="col">
+					<div className="col">
+						<Button variant={'primary'} className="btn-lg mt-5">Créer un sujet</Button>
 						<CardContainer asRow title="Derniers sujets">
 							<div>
-							<div className="card">
+							<div className="card ml-2 mr-2">
 								<div className="card-content">
 									<div className="media">
-									<div className="media-left">
-										<figure className="image is-48x48">
-										<img src="https://bulma.io/images/placeholders/96x96.png" alt=""/>
-										</figure>
-									</div>
-									<div className="media-content">
-										<p className="title is-6">John Smith</p>
-									</div>
+										<img className="rounded-circle mr-3" src="https://bulma.io/images/placeholders/64x64.png" alt=""/>
+										<div className="card-title">John Smith</div>
 									</div>
 								</div>
-								<div className="content ml-2">
+								<div className="card-text ml-2">
 									Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 									Phasellus nec iaculis mauris
 									<br/>
-									<time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+									<p className="card-text"><small className="text-muted">11:09 PM - 1 Jan 2016</small></p>
+								
 								</div>
 							</div>
 							</div>
