57988b35d81139fdd3f91c04270260b57cc6bc40
Mathis Laroche <ecomath360@gmail.com>
2021-10-10 13:52:17 -0400

    Init branch to work on album

---
+++ frontend/src/Components/CourseComponents/MDEditor/remark-album.ts
@@ -0,0 +1,20 @@
+/**
+ * ALBUM
+ * (AL)ive (B)eautiful (U)timate (M)arkdown
+ */
+
+import { albumOptionsType } from './albumTypes';
+
+//export function move(options: albumOptionsType) {
+//	if (!extname) {
+//		throw new Error('Missing `extname` in options');
+//	}
+//
+//	return (tree, file) => {
+//		if (file.extname && file.extname !== extname) {
+//			file.extname = extname;
+//		}
+//	}
+//}
+
+export {};
