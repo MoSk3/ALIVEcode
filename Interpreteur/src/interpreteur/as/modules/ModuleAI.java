eb06e747f821ced540bab57d87a338416574ab6a
Enric Soldevila <62816157+MoSk3@users.noreply.github.com>
2021-10-10 09:08:13 -0400

    Ai fix conflicts (#57)
    * FIX - Ai merge conflicts
    
    Co-authored-by: Félix Jobin <fjobin007@gmail.com>
    Co-authored-by: Mathis Laroche <ecomath360@gmail.com>

--- Interpreteur/src/interpreteur/as/modules/ModuleAI.java
+++ Interpreteur/src/interpreteur/as/modules/ModuleAI.java
@@ -434,12 +434,9 @@ public class ModuleAI {
                         return new Nul();
                     }
                 },
-<<<<<<< HEAD
                 /*
                     Evaluates the regression on the graph at a specific x value.
                  */
-=======
->>>>>>> origin/dev
                 new ASObjet.Fonction("evaluer", new ASObjet.Fonction.Parametre[]{
                         new ASObjet.Fonction.Parametre(
                                 ASObjet.TypeBuiltin.nombre.asType(), "x", null)
@@ -458,7 +455,6 @@ public class ModuleAI {
                         }
                         return new Decimal(res);
                     }
-<<<<<<< HEAD
                 },
                 /*
                     a
@@ -470,8 +466,6 @@ public class ModuleAI {
                         executeurInstance.addData(new Data(Data.Id.FONCTION_COUT));
                         return new Nul();
                     }
-=======
->>>>>>> origin/dev
                 }
         }, new ASObjet.Variable[]{});
     }
