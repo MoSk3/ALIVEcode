fd1cfd72188943994f94d0b1765e8d83c0087502
Mathis Laroche <ecomath360@gmail.com>
2021-10-21 16:37:31 -0400

    aaa

---
+++ frontend/src/Pages/Level/LevelExecutor2.ts
@@ -0,0 +1,5 @@
+import axios from 'axios';
+import { CMD } from '../../Components/LevelComponents/Cmd/cmdTypes';
+import { User } from '../../Models/User/user.entity';
+
+export abstract class AbstractLevelExecutor {}
