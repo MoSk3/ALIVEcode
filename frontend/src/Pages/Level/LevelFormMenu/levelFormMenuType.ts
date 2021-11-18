b52d1b92684f736215c040ab9de6d11dc13c511e
Enric <mosk3002@gmail.com>
2021-11-18 12:11:23 -0500

    FIX - Forms css bugs

---
+++ frontend/src/Pages/Level/LevelFormMenu/levelFormMenuType.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Row } from 'react-bootstrap';
+
+export const StyledLevelFormMenu = styled(Row)`
+	justify-content: center;
+	width: 100%;
+	height: 100%;
+`;
